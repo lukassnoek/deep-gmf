5abb21f97d438a24cc7dd6b8b1bc65bbf8c2b59d
Lukas Snoek <lukassnoek@gmail.com>
2022-02-11 15:44:39 +0000

    Update stim gen revamp

---
+++ scripts/generate_stimuli/generate_manyIDsingleIMGnovariation.sh
@@ -0,0 +1,7 @@
+python scripts/generate_faces.py \
+    --out-dir /analyse/Project0257/lukas/data/gmf_manyIDsingleIMGnovariation \
+    --save-image-separately \
+    --image-format png \
+    --n-id 65536 \
+    --ethns WC \
+    --save-id-params
